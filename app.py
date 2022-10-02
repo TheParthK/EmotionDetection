d4e0f963cdc20ecd3c677f37e494f0f726d3f0b7
Parth Kalia <95091403+TheParthK@users.noreply.github.com>
2022-10-02 11:13:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,34 +22,35 @@ a = st.empty()
 face_cascade = cv2.CascadeClassifier('haarcascade.xml')
 # a.text_area(value='detecting', label='')
 while True:
-    ret, frame = cam.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_window2.image(frame)
+    if frame or frame.any():
+        ret, frame = cam.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_window2.image(frame)
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        frame = frame[y:y+h, x:x+h]
-
-
-
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            frame = frame[y:y+h, x:x+h]
 
 
-    if frame.any():
-        resized = cv2.resize(frame, (48, 48))
-        resized_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-        reshaped_array = np.reshape(resized_gray, (1, 48, 48, 1))
-        result=trained_model.predict(reshaped_array)
-        y_pred=np.argmax(result[0])
 
-        pred = classes[y_pred]
-        # print('The person facial emotion is:',classes[y_pred])
-        a.write(pred)
-    # a = st.empty()
-    
 
-    frame_window.image(resized_gray)
+
+        if frame.any():
+            resized = cv2.resize(frame, (48, 48))
+            resized_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+            reshaped_array = np.reshape(resized_gray, (1, 48, 48, 1))
+            result=trained_model.predict(reshaped_array)
+            y_pred=np.argmax(result[0])
+
+            pred = classes[y_pred]
+            # print('The person facial emotion is:',classes[y_pred])
+            a.write(pred)
+        # a = st.empty()
+
+
+        frame_window.image(resized_gray)
 
 
 
